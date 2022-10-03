42005f66717fcd9ac2f5d9e358680aa1dd90596c
Lab01 <lab01@ifpi.edu>
2022-10-03 17:06:17 -0300

    alterações no layout da pagina home

--- lib/main.dart
+++ lib/main.dart
@@ -18,71 +18,90 @@ class _MyHomeState extends State<MyHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HOME'), centerTitle: true,
-      actions: [Icon(Icons.shopping_cart)],),
-      body: Column(children: [
-        TextField(
-          decoration: InputDecoration(
-                border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40),
-    ),
-            hintText: 'Categoria',
-            hintStyle: TextStyle(
-              color: Colors.teal.withOpacity(0.7),
-              fontWeight: FontWeight.bold,
-              fontSize: 20, ),
-            suffixIcon: Icon(
-              Icons.search,
-              color: Colors.teal.withOpacity(0.7),
-              size: 34,
-            ),
-            enabledBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
+        title: Text('HOME'),
+        centerTitle: true,
+        actions: [Icon(Icons.shopping_cart)],
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            decoration: InputDecoration(
+                      labelText: "Busca",
+                      hintText: "Buscar Serviço",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20.0)))),
           ),
         ),
-        SizedBox(
-          height: 100,
-          child: 
-           GridView(
+       ElevatedButton.icon(
+                    onPressed: () {},
+                    label: Text('Categorias'),
+                    icon: Icon(Icons.apps), 
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 10.0),
+                      primary: Color.fromRGBO(0, 226, 187, 8),
+                      shape: StadiumBorder(),
+                    ),
+                    
+                  ),
+        Text('Pedidos com boas avaliações'),
+        Expanded(
+          child: GridView(
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 8,
                 mainAxisSpacing: 8,
-                childAspectRatio: 4), 
+                childAspectRatio: 1),
             children: [
-              
+              Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(
+                        'https://www.pocosja.com.br/divirta-se/wp-content/uploads/2017/09/DSC0415.jpg'),
+                    Text('Comida Mineira'),
+                    Text('120.00'),
+                  ],
+                ),
+              ),
+              Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(
+                        'https://saude.abril.com.br/wp-content/uploads/2016/11/cachorro-molhado.jpg'),
+                    Text('Banho De Tosa'),
+                    Text('110.00'),
+                  ],
+                ),
+              ),
+              Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(
+                        'https://uploads.metropoles.com/wp-content/uploads/2021/11/12165909/Perfumaria_O_Boticario.jpg'),
+                    Text('Lily'),
+                    Text('240.00'),
+                  ],
+                ),
+              ),
+              Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(
+                        'https://listenx.com.br/blog/wp-content/uploads/2022/02/textos-comerciais-para-lojas-de-roupas.png'),
+                    Text('Blazer'),
+                    Text('480.00'),
+                  ],
+                ),
+              )
             ],
           ),
-        ),
-        TextButton(onPressed: (() {}), child: Text('Mais')),
-
-        Expanded(
-          child: GridView(gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 8,
-                  mainAxisSpacing: 8,
-                  childAspectRatio: 1),children: [
-                    Card(child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                      Image.network('https://www.pocosja.com.br/divirta-se/wp-content/uploads/2017/09/DSC0415.jpg'),
-                      Text('Comida Mineira'),
-                      Text('120.00'),
-                    ],),),
-                    Card(child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                      Image.network('https://saude.abril.com.br/wp-content/uploads/2016/11/cachorro-molhado.jpg'),
-                      Text('Banho De Tosa'),
-                      Text('110.00'),
-                    ],),),
-                    Card(child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                      Image.network('https://uploads.metropoles.com/wp-content/uploads/2021/11/12165909/Perfumaria_O_Boticario.jpg'),
-                      Text('Lily'),
-                      Text('240.00'),
-                    ],),),
-                    Card(child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                      Image.network('https://listenx.com.br/blog/wp-content/uploads/2022/02/textos-comerciais-para-lojas-de-roupas.png'),
-                      Text('Blazer'),
-                      Text('480.00'),
-                    ],),)
-                  ],),
         )
       ]),
       drawer: Drawer(
@@ -104,7 +123,6 @@ class _MyHomeState extends State<MyHome> {
           ],
         ),
       ),
-
     );
   }
 }
